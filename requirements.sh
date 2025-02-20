422177d5608510fc429ccb56d7557d7392e71ae9
DeveloperPratim <130478952+DeveloperPratim@users.noreply.github.com>
2025-02-20 09:16:07 +0530

    Ok

---
+++ requirements.sh
@@ -0,0 +1,6 @@
+pip install googlesearch-python
+pip install autoawq
+pip install pandas openpyxl
+pip install git+https://github.com/huggingface/transformers.git@72958fcd3c98a7afdc61f953aa58c544ebda2f79
+pip install git+https://github.com/casper-hansen/AutoAWQ.git@1c5ccc791fa2cb0697db3b4070df1813f1736208
+pip install gradio
